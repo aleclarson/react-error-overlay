9965f423d662c4063cd6d018050f51849a30eb99
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2021-03-05 11:07:54 -0500

    6.0.9

---
+++ webpack.config.iframe.js
@@ -0,0 +1,62 @@
+/**
+ * Copyright (c) 2015-present, Facebook, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+'use strict';
+
+const path = require('path');
+const webpack = require('webpack');
+const TerserPlugin = require('terser-webpack-plugin');
+
+module.exports = {
+  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+  entry: './src/iframeScript.js',
+  output: {
+    path: path.join(__dirname, './lib'),
+    filename: 'iframe-bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        oneOf: [
+          // Source
+          {
+            test: /\.js$/,
+            include: [path.resolve(__dirname, './src')],
+            use: {
+              loader: 'babel-loader',
+            },
+          },
+          // Dependencies
+          {
+            test: /\.js$/,
+            exclude: /@babel(?:\/|\\{1,2})runtime/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                babelrc: false,
+                configFile: false,
+                compact: false,
+                presets: [
+                  ['babel-preset-react-app/dependencies', { helpers: true }],
+                ],
+              },
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      // We set process.env.NODE_ENV to 'production' so that React is built
+      // in production mode.
+      'process.env': { NODE_ENV: '"production"' },
+      // This prevents our bundled React from accidentally hijacking devtools.
+      __REACT_DEVTOOLS_GLOBAL_HOOK__: '({})',
+    }),
+  ],
+  performance: false,
+};
